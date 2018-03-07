07b7a557844235d4e26d538d348fd232b19202cc
rytoj <2600kg@gmail.com>
2018-03-07 23:07:23 +0200

    Refactor code

--- star.py
+++ star.py
@@ -1,84 +1,130 @@
 import requests
+import time
 from stellar_base.address import Address
 from stellar_base.builder import Builder
 from stellar_base.keypair import Keypair
+import os
 
 
-def generate_new_prv_key():
-    """
-    Generates new private key (or seed)
-    :return:
-    """
-    kp = Keypair.random()
-    private_key = kp.seed().decode()
-    return private_key
+class GetKeys:
+    def generate_new_prv_key(self):
+        """
+        Generates new private key (or seed)
+        :return:
+        """
+        kp = Keypair.random()
+        private_key = kp.seed().decode()
+        return private_key
+
+    def get_public_key_from_seed(self, seed):
+        """
+        Get public key from seed
+        :param seed: private key
+        :return: public address
+        """
+        kp = Keypair.from_seed(seed)
+        publickey = kp.address().decode()
+        return publickey
+
+    def get_private_public_key(self):
+        prv_key = self.generate_new_prv_key()
+        pub_key = self.get_public_key_from_seed(prv_key)
+        print("Public key: {}\nPrivate key:{}".format(prv_key, pub_key))
 
 
-def get_public_key_from_seed(seed):
-    """Get public key from seed
-    :param seed: private key
-    :return: public address
-    """
-    kp = Keypair.from_seed(seed)
-    publickey = kp.address().decode()
-    return publickey
+class Stelar():
+    def __init__(self, publickey, testnet=False, local=False):
+        self.pulic_key = publickey
+        self.testnet = testnet
+        self.local = local
 
+    def get_wallet(self):
+        """"Get balance from public key
+        :param public_key: stellar public key
+        :param testnet: boolean parameter
+        :return: current balance
+        """
 
-def get_balance(public_key, testnet=False):
-    """"Get balance from public key
-    :param public_key: stellar public key
-    :param testnet: boolean parameter
-    :return: current balance
-    """
-    try:
-        r = requests.get('http://localhost:8000/friendbot?addr=' + public_key)  # Local docker container
-    except requests.exceptions.ConnectionError:
-        r = requests.get('https://horizon-testnet.stellar.org/friendbot?addr=' + public_key)
-    if testnet:
-        address = Address(address=public_key)
-    else:
-        address = Address(address=public_key, network='public')
+        if self.local:
+            try:
+                r = requests.get('http://localhost:8000/friendbot?addr=' + self.pulic_key, timeout=1)
+            except requests.exceptions.ConnectionError:
+                r = requests.get('https://horizon-testnet.stellar.org/friendbot?addr=' + self.pulic_key)
+        else:
+            r = requests.get('https://horizon-testnet.stellar.org/friendbot?addr=' + self.pulic_key)
 
-    address.get()  # get the updated information
-    return public_key, address.balances[0]['balance']
+        if self.testnet:
+            self.address = Address(address=self.pulic_key)
+        else:
+            self.address = Address(address=self.pulic_key, network='public')
 
+        self.address.get()  # get the updated information
+        self.wallet = self.address.balances
+        return self.address.balances
 
-def send_stellar(seed, public_key, amount, comment="", testnet=False):
-    """
-    Sends stellar to public address
-    :param testnet: boolean parameter
-    :param seed: private key
-    :param amount: amount you want to send
-    :param comment: comment in transaction
-    :param public_key: public key, where you want to send lumen
-    :return: return transaction info
-    """
-    if testnet:
-        builder = Builder(secret=seed)
-    else:
-        builder = Builder(secret=seed, network='public')
-    builder.add_text_memo(comment)  # string length <= 28 bytes
-    builder.append_payment_op(public_key, amount, 'XLM')
-    builder.sign()
-    return builder.submit()
+    def get_native_balance(self):
+        """
+        :return: XLM balance
+        """
+        for asset in self.wallet:
+            if asset.get("asset_type") == "native":
+                return asset.get("balance")
+
+    def get_token_balance(self, token):
+        """
+        :return: token balance
+        """
+        for asset in self.wallet:
+            if asset.get('asset_code') == token:
+                return asset.get("balance") + " XML"
+        return "Token not found."
+
+    def get_balance(self, balance):
+        return balance.get("balance")
+
+    def loop_balance(self, refresh=3, token=False):
+        """
+        :param refresh: default refresh time
+        :param token: token name
+        :return: asset balance
+        """
+        while True:
+            time.sleep(5)
+            self.get_wallet()
+            os.system('cls' if os.name == 'nt' else 'clear')
+            if not token:
+                print(self.get_native_balance() + " XLM")
+            else:
+                print(self.get_token_balance(token))
+
+    def send(self, seed, public_key, amount, comment="", asset="XML"):
+        """
+        Sends stellar to public address
+        :param testnet: boolean parameter
+        :param seed: private key
+        :param amount: amount you want to send
+        :param comment: comment in transaction string length <= 28 bytes
+        :param public_key: public key, where you want to send lumen
+        :param asset: by default lumen XML
+        :return: return transaction info
+        """
+        if self.testnet:
+            builder = Builder(secret=seed)
+        else:
+            builder = Builder(secret=seed, network='public')
+        builder.add_text_memo(comment)
+        builder.append_payment_op(public_key, amount, asset)
+        builder.sign()
+        return builder.submit()
 
 
 def run_as_standalone():
-    # public keys
-    # ('GCPFZJIO47RPSPVRCPNLJE7CEMJJXN5YRQLI2BWMSGRVBTCG4ZOX3XRJ', '9598.9999400')
-    # ('GDYEGCJSQKSJIZ2LJJHKWV244X7W2F7OWVYZYF7FQZC4OSQRS53G7CXF', '10401.0000000')
+    account = Stelar(publickey="GABGKZCXBGDOPBFNWOHLOIJU4DFA3VYKWMPJAMKV4QS4YYJ5IK3CNWNQ", testnet=True)
+    wallet = account.get_wallet()
+    stellar = account.get_native_balance()
+    sudocoin = account.get_token_balance("sudokoin")
+    account.loop_balance()
 
-    seed1 = 'SBUWFJKAREJ3HT6VIAS6L6IF6OCXBLSAM7PWWEM5R64KH5KLJG6PLRCU'
-    seed2 = 'SB3UCTCYLDNMQTA2ZGBISGTMI7TFJQ3ZZXU3EUNO27HGYZW5GCMR7CC4'
-    publickey1 = get_public_key_from_seed(seed=seed1)
-    publickey2 = get_public_key_from_seed(seed=seed2)
-
-    transaction = send_stellar(seed=seed1, public_key=publickey2, amount=1, comment="Test message", testnet=True)
-    print(transaction)
-    # Print balances
-    for pub in publickey1, publickey2:
-        balance = get_balance(pub, testnet=True)
-        print(balance)
 
 
 if __name__ == "__main__":
